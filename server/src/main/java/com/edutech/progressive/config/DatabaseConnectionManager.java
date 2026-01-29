f8764a3c362ef2ae3e8395ca9c42ff8860a0e8f6
rutvalthonge4312 <dummy@gmail.com>
2026-01-29 18:40:26 +0000

    Day - 1

--- server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -1,5 +1,32 @@
 package com.edutech.progressive.config;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DatabaseConnectionManager {
+
+    private static final Properties properties =  new Properties();
+
+    private static void loadProperties(){
+        try{
+            properties.load(new FileInputStream("application.properties"));
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection()throws SQLException{
+        loadProperties();
+        String user=properties.getProperty("spring.datasource.username");
+        String pass=properties.getProperty("spring.datasource.password");
+        String url=properties.getProperty("spring.datasource.url");
+
+        return DriverManager.getConnection(url,user,pass);
+
+    }
 
 }
